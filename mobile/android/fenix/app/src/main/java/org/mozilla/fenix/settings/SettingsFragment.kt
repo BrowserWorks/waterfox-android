88331d8d96e2f5b41b082f186dc9c6f8392515b9
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-02-04 11:25:45 +0000

    feat(privacy): disable metrics service and remove data choices preference option

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -473,10 +473,6 @@ class SettingsFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFragment 
                     null
                 }
 
-                resources.getString(R.string.pref_key_data_choices) -> {
-                    SettingsFragmentDirections.actionSettingsFragmentToDataChoicesFragment()
-                }
-
                 // Advanced preferences
                 resources.getString(R.string.pref_key_addons) -> {
                     Addons.openAddonsInSettings.record(NoExtras())

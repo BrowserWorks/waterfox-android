be9d5bd6d4e4159e3e508e1f244380ebeb4a0947
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-02-04 19:37:43 +0000

    feat(nimbus): disable Nimbus feature flags and handle Nimbus exceptions safely

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -749,7 +749,12 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity, Crash
                 MessageNotificationWorker.setMessageNotificationWorker(applicationContext)
             }
 
-            if (components.core.sentFromFirefoxManager.shouldShowSnackbar) {
+            val shouldShowSnackbar = try {
+                components.core.sentFromFirefoxManager.shouldShowSnackbar
+            } catch (e: org.mozilla.experiments.nimbus.internal.NimbusFeatureException) {
+                false
+            }
+            if (shouldShowSnackbar) {
                 components.appStore.dispatch(ShareAction.ShareToWhatsApp)
             }
         }
@@ -1500,7 +1505,11 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity, Crash
 
     private suspend fun showFullscreenMessageIfNeeded(context: Context) {
         val messaging = context.components.nimbus.messaging
-        val nextMessage = messaging.getNextMessage(FenixMessageSurfaceId.SURVEY) ?: return
+        val nextMessage = try {
+            messaging.getNextMessage(FenixMessageSurfaceId.SURVEY)
+        } catch (e: org.mozilla.experiments.nimbus.internal.NimbusFeatureException) {
+            return
+        } ?: return
         val researchSurfaceDialogFragment = ResearchSurfaceDialogFragment.newInstance(
             keyMessageText = nextMessage.text,
             keyAcceptButtonText = nextMessage.buttonLabel,

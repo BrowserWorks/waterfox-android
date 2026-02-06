a7d454324e2e4f30d588d57f395c8b153975806b
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-02-06 17:02:59 +0000

    feat(settings): hide disable preferences from settings UI and allows enable secret settings

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -576,15 +576,14 @@ class SettingsFragment : PreferenceFragmentCompat() {
             )?.isVisible = showSecretDebugMenuThisSession
             findPreference<Preference>(
                 getPreferenceKey(R.string.pref_key_secret_debug_info),
-            )?.isVisible = showSecretDebugMenuThisSession
+            )?.isVisible = false
             findPreference<Preference>(
                 getPreferenceKey(R.string.pref_key_sync_debug),
             )?.isVisible = showSecretDebugMenuThisSession
             findPreference<Preference>(
                 getPreferenceKey(R.string.pref_key_firefox_labs),
             )?.isVisible = enableFirefoxLabs
-            preferenceStartProfiler?.isVisible = showSecretDebugMenuThisSession &&
-                (components.core.engine.profiler?.isProfilerActive() != null)
+            preferenceStartProfiler?.isVisible = false
         }
         setupCookieBannerPreference(settings)
         setupInstallAddonFromFilePreference(settings)

097fc33cefd1a37d47cf00d707e888b7fa425ba0
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-02-06 17:02:59 +0000

    feat(settings): hide disabled preferences from settings UI and allows enable secret settings

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/CustomizationFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/CustomizationFragment.kt
@@ -125,7 +125,6 @@ class CustomizationFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFrag
         // preference is not shown
         setupGesturesCategory(
             isSwipeToolbarToSwitchTabsVisible = !tabletAndTabStripEnabled,
-            isSummarizationEnabled = status.isSummarizationFeatureEnabled,
             isSummarizationGestureEnabled = status.isSummarizationGestureEnabled,
         )
     }
@@ -360,7 +359,6 @@ class CustomizationFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFrag
 
     private fun setupGesturesCategory(
         isSwipeToolbarToSwitchTabsVisible: Boolean,
-        isSummarizationEnabled: Boolean,
         isSummarizationGestureEnabled: Boolean,
     ) {
         requirePreference<SwitchPreferenceCompat>(R.string.pref_key_website_pull_to_refresh).apply {
@@ -382,8 +380,7 @@ class CustomizationFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFrag
             onPreferenceChangeListener = SharedPreferenceUpdater()
         }
         requirePreference<SwitchPreferenceCompat>(R.string.pref_key_shake_gesture_enabled).apply {
-            isVisible = context.components.settings.shakeToSummarizeFeatureFlagEnabled &&
-                    isSummarizationEnabled
+            isVisible = false
             isChecked = isSummarizationGestureEnabled
             onPreferenceChangeListener = { _, newValue ->
                 val updatedValue = (newValue as? Boolean) ?: false
--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -202,15 +202,15 @@ class SettingsFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFragment 
 
         findPreference<Preference>(
             getPreferenceKey(R.string.pref_key_translation),
-        )?.isVisible = components.core.store.state.translationEngine.isEngineSupported == true
+        )?.isVisible = components.core.store.state.translationEngine.isEngineSupported != false
 
         findPreference<Preference>(
             getPreferenceKey(R.string.pref_key_page_summaries),
-        )?.isVisible = components.settings.shakeToSummarizeFeatureFlagEnabled
+        )?.isVisible = false
 
         findPreference<Preference>(
             getPreferenceKey(R.string.pref_key_ai_controls),
-        )?.isVisible = requireComponents.settings.aiControlsFeatureFlagEnabled
+        )?.isVisible = false
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -417,14 +417,6 @@ class SettingsFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFragment 
                 SettingsFragmentDirections.actionSettingsFragmentToTranslationsSettingsFragment()
             }
 
-            resources.getString(R.string.pref_key_page_summaries) -> {
-                SettingsFragmentDirections.actionSettingsFragmentToPageSummariesSettingsFragment()
-            }
-
-            resources.getString(R.string.pref_key_ai_controls) -> {
-                SettingsFragmentDirections.actionSettingsFragmentToAiControlsFragment()
-            }
-
             // Privacy and security preferences
             resources.getString(R.string.pref_key_private_browsing) -> {
                 SettingsFragmentDirections.actionSettingsFragmentToPrivateBrowsingFragment()
@@ -628,12 +620,11 @@ class SettingsFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFragment 
             )?.isVisible = showSecretDebugMenuThisSession
             findPreference<Preference>(
                 getPreferenceKey(R.string.pref_key_sync_debug),
-            )?.isVisible = showSecretDebugMenuThisSession
+            )?.isVisible = showSecretDebugMenuThisSession && !Config.channel.isRelease
             findPreference<Preference>(
                 getPreferenceKey(R.string.pref_key_firefox_labs),
             )?.isVisible = enableFirefoxLabs
-            preferenceStartProfiler?.isVisible = showSecretDebugMenuThisSession &&
-                (components.core.engine.profiler?.isProfilerActive() != null)
+            preferenceStartProfiler?.isVisible = false
         }
         setupInstallAddonFromFilePreference(settings)
         setLinkSharingPreference()
@@ -735,7 +726,7 @@ class SettingsFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFragment 
         val preferenceEnabledGeckoLogs =
             findPreference<Preference>(getPreferenceKey(R.string.pref_key_enable_gecko_logs))
 
-        val show = settings.showSecretDebugMenuThisSession
+        val show = settings.showSecretDebugMenuThisSession && !Config.channel.isRelease
         preferenceEnabledGeckoLogs?.isVisible = show
 
         preferenceEnabledGeckoLogs?.onPreferenceChangeListener =

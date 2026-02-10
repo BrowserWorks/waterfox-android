1914612e23b1b2d37dd8a3d959bc9f2f05a5c003
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-02-10 11:10:21 +0000

    feat(addons): allow browsing the full AMO website, disable remote connections and remove default addon collections

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/middleware/MenuDialogMiddleware.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/middleware/MenuDialogMiddleware.kt
@@ -190,19 +190,7 @@ class MenuDialogMiddleware(
                 return@launch
             }
 
-            val recommendedAddons = addons
-                .filter { !it.isInstalled() }
-                .shuffled()
-                .take(NUMBER_OF_RECOMMENDED_ADDONS_TO_SHOW)
 
-            if (recommendedAddons.isNotEmpty()) {
-                store.dispatch(
-                    MenuAction.UpdateExtensionState(
-                        recommendedAddons = recommendedAddons,
-                    ),
-                )
-                store.dispatch(MenuAction.UpdateShowExtensionsOnboarding(true))
-            }
         } catch (e: AddonManagerException) {
             logger.error("Failed to query extensions", e)
         }

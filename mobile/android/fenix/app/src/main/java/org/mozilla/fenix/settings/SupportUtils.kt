4a3ef0e2635f0f0367c3346b5657b34ffcf6d0c5
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-02-13 15:49:53 +0000

    feat(support): redirect to Waterfox support URLs

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SupportUtils.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SupportUtils.kt
@@ -26,8 +26,8 @@ object SupportUtils {
     const val GOOGLE_URL = "https://www.google.com/"
     const val GOOGLE_US_URL = "https://www.google.com/webhp?client=firefox-b-1-m&channel=ts"
     const val GOOGLE_XX_URL = "https://www.google.com/webhp?client=firefox-b-m&channel=ts"
-    const val WHATS_NEW_URL = "https://www.mozilla.org/firefox/android/notes"
-    const val FXACCOUNT_SUMO_URL = "https://support.mozilla.org/kb/access-mozilla-services-firefox-account"
+    const val WHATS_NEW_URL = "https://www.waterfox.com/releases/android/${BuildConfig.VERSION_NAME}"
+    const val FXACCOUNT_SUMO_URL = "https://www.waterfox.com/support/access-mozilla-services-firefox-account"
     const val ANDROID_SUPPORT_SUMO_URL = "mzl.la/AndroidSupport"
 
     // This is locale-less on purpose so that the content negotiation happens on the AMO side because the current
@@ -39,29 +39,29 @@ object SupportUtils {
         HELP("faq-android"),
         PRIVATE_BROWSING_MYTHS("common-myths-about-private-browsing"),
         YOUR_RIGHTS("your-rights"),
-        TRACKING_PROTECTION("tracking-protection-firefox-android"),
+        TRACKING_PROTECTION("tracking-protection-waterfox-android"),
         TOTAL_COOKIE_PROTECTION("enhanced-tracking-protection-android"),
-        OPT_OUT_STUDIES("how-opt-out-studies-firefox-android"),
+        OPT_OUT_STUDIES("how-opt-out-studies-waterfox-android"),
         REMOTE_IMPROVEMENTS("remote-improvements"),
         SEND_TABS("send-tab-preview"),
-        SET_AS_DEFAULT_BROWSER("make-firefox-default-browser-android"),
-        SEARCH_SUGGESTION("how-search-firefox-preview"),
+        SET_AS_DEFAULT_BROWSER("make-waterfox-default-browser-android"),
+        SEARCH_SUGGESTION("how-search-waterfox-preview"),
         CUSTOM_SEARCH_ENGINES("custom-search-engines"),
-        SYNC_SETUP("how-set-firefox-sync-firefox-android"),
+        SYNC_SETUP("how-set-waterfox-sync-waterfox-android"),
         SMARTBLOCK("smartblock-enhanced-tracking-protection"),
         SPONSOR_PRIVACY("sponsor-privacy"),
-        HTTPS_ONLY_MODE("https-only-mode-firefox-android"),
-        DNS_OVER_HTTPS("configure-dns-over-https-protection-levels-firefox-android"),
+        HTTPS_ONLY_MODE("https-only-mode-waterfox-android"),
+        DNS_OVER_HTTPS("https-only-mode-waterfox-android"),
         DNS_OVER_HTTPS_LOCAL_PROVIDER(
-            "configure-dns-over-https-protection-levels-firefox-android#w_what-is-a-local-provider",
+            "https-only-mode-waterfox-android",
         ),
-        DNS_OVER_HTTPS_NETWORK("configure-dns-over-https-protection-levels-firefox-android"),
+        DNS_OVER_HTTPS_NETWORK("https-only-mode-waterfox-android"),
         UNSIGNED_ADDONS("unsigned-addons"),
-        FX_SUGGEST("search-suggestions-firefox"),
+        FX_SUGGEST("search-suggestions-waterfox"),
         TRANSLATIONS("android-translation"),
         MANAGE_OPTIONAL_EXTENSION_PERMISSIONS("manage-optional-permissions-android-extensions"),
         EXTENSION_PERMISSIONS("extension-permissions"),
-        FIND_INSTALL_ADDONS("add-ons-firefox-android"),
+        FIND_INSTALL_ADDONS("add-ons-waterfox-android"),
         CRASH_REPORTS("mobile-crash-reports"),
         TECHNICAL_AND_INTERACTION_DATA("mobile-technical-and-interaction-data"),
         USAGE_PING_SETTINGS("usage-ping-settings-mobile"),
@@ -76,11 +76,11 @@ object SupportUtils {
     }
 
     enum class MozillaPage(internal val path: String) {
-        PRIVACY_NOTICE("privacy/firefox/"),
+        PRIVACY_NOTICE("docs/policies/privacy/"),
         PRIVACY_NOTICE_UPDATE("${PRIVACY_NOTICE.path}update/"),
         PRIVACY_NOTICE_NEXT("${PRIVACY_NOTICE.path}next/"),
         MANIFESTO("about/manifesto/"),
-        TERMS_OF_SERVICE("about/legal/terms/firefox/"),
+        TERMS_OF_SERVICE("docs/policies/terms/"),
     }
 
     /**
@@ -93,16 +93,8 @@ object SupportUtils {
         useMobilePage: Boolean = true,
     ): String {
         val escapedTopic = getEncodedTopicUTF8(topic.topicStr)
-        // Remove the whitespace so a search is not triggered:
-        val appVersion = context.appVersionName.replace(" ", "")
-        val osTarget = "Android"
-        val langTag = getLanguageTag(locale)
-        val platform = if (useMobilePage) {
-            "mobile"
-        } else {
-            "firefox"
-        }
-        return "https://support.mozilla.org/1/$platform/$appVersion/$osTarget/$langTag/$escapedTopic"
+        val osTarget = "android"
+        return "https://www.waterfox.com/support/$osTarget/$escapedTopic"
     }
 
     /**
@@ -111,14 +103,12 @@ object SupportUtils {
      */
     fun getGenericSumoURLForTopic(topic: SumoTopic, locale: Locale = Locale.getDefault()): String {
         val escapedTopic = getEncodedTopicUTF8(topic.topicStr)
-        val langTag = getLanguageTag(locale)
-        return "https://support.mozilla.org/$langTag/kb/$escapedTopic"
+        return "https://www.waterfox.com/support/$escapedTopic"
     }
 
     fun getMozillaPageUrl(page: MozillaPage, locale: Locale = Locale.getDefault()): String {
         val path = page.path
-        val langTag = getLanguageTag(locale)
-        return "https://www.mozilla.org/$langTag/$path"
+        return "https://www.waterfox.com/$path"
     }
 
     fun createCustomTabIntent(context: Context, url: String): Intent = CustomTabsIntent.Builder()

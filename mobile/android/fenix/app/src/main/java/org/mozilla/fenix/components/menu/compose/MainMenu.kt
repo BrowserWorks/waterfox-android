75e893cf1fd618e7a87a09fd4a0850027f9af63d
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-02-15 18:52:45 +0000

    feat(misc): add "What's New" menu item

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/MainMenu.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/MainMenu.kt
@@ -67,6 +67,7 @@ import org.mozilla.fenix.theme.PreviewThemeProvider
 import org.mozilla.fenix.theme.Theme
 import org.mozilla.fenix.theme.ThemedValue
 import org.mozilla.fenix.theme.ThemedValueProvider
+import org.mozilla.fenix.whatsnew.WhatsNew
 import mozilla.components.ui.icons.R as iconsR
 
 /**
@@ -158,6 +159,7 @@ fun MainMenu(
     onCustomizeReaderViewMenuClick: () -> Unit,
     onMozillaAccountButtonClick: () -> Unit,
     onSettingsButtonClick: () -> Unit,
+    onWhatsNewMenuClick: () -> Unit = {},
     onBookmarkPageMenuClick: () -> Unit,
     onEditBookmarkButtonClick: () -> Unit,
     onSwitchToDesktopSiteMenuClick: () -> Unit,
@@ -181,6 +183,9 @@ fun MainMenu(
     moreSettingsSubmenu: @Composable () -> Unit,
     extensionSubmenu: @Composable () -> Unit,
 ) {
+    val context = LocalContext.current
+    val shouldHighlightWhatsNew = WhatsNew.shouldHighlightWhatsNew(context)
+
     MenuFrame(
         contentModifier = Modifier
             .padding(
@@ -329,6 +334,15 @@ fun MainMenu(
                 isPrivate = isPrivate,
                 onClick = onMozillaAccountButtonClick,
             )
+
+            if (shouldHighlightWhatsNew) {
+                MenuItem(
+                    label = stringResource(id = R.string.browser_menu_whats_new),
+                    beforeIconPainter = painterResource(id = iconsR.drawable.mozac_ic_whats_new_24),
+                    isBeforeIconHighlighted = true,
+                    onClick = onWhatsNewMenuClick,
+                )
+            }
 
             MenuItem(
                 label = stringResource(id = R.string.browser_menu_settings),

54242d4b86ca71d24b8783cca6f9f33211e37d73
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-02-17 10:03:23 +0000

    feat(theme): migrate old themes

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/FenixApplication.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/FenixApplication.kt
@@ -310,6 +310,7 @@ open class FenixApplication : Application(), Provider, ThemeProvider {
         // Note: This is a historical artifact and should be revisited.
         val store = components.core.store
 
+
         // StartupMetrics accesses shared preferences so do this off thread.
         @OptIn(DelicateCoroutinesApi::class)
         GlobalScope.launch(IO) {
@@ -320,6 +321,7 @@ open class FenixApplication : Application(), Provider, ThemeProvider {
         // before for its completion synchronously.
         val megazordDeferred = setupMegazordNetwork()
 
+        settings().migrateLegacyThemePreferencesIfNeeded()
         setDayNightTheme()
         components.strictMode.enableStrictMode(true)
 

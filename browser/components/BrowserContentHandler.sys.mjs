840dbbe4962dd3d4e654fa7cbc3e21dc09bea2cb
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-02-17 12:46:10 +0000

    feat(misc): enable JPEG-XL support in GeckoView and build configuration

--- browser/components/BrowserContentHandler.sys.mjs
+++ browser/components/BrowserContentHandler.sys.mjs
@@ -1346,6 +1346,7 @@ function maybeRecordToHandleTelemetry(uri, isLaunch) {
       ".xhtml",
       ".svg",
       ".webp",
+      ".jxl",
     ]);
     if (registeredExtensions.has(extension)) {
       counter[extension].add(1);

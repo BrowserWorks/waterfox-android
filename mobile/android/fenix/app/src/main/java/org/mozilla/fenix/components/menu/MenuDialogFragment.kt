b19dc280dabe114540b368d8f62b5bf4abf22cc3
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-02-18 16:50:53 +0000

    fix(misc): compose state inference

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/MenuDialogFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/MenuDialogFragment.kt
@@ -557,7 +557,7 @@ class MenuDialogFragment : BottomSheetDialogFragment() {
                         store.stateFlow.map { state -> state.ipProtectionMenuState }
                     }.collectAsState(initial = store.state.ipProtectionMenuState)
 
-                    val contentState: Route by remember { mutableStateOf(initRoute) }
+                    val contentState: Route by remember { mutableStateOf<Route>(initRoute) }
 
                     var shouldShowMenuBanner by
                     remember { mutableStateOf(settings.shouldShowMenuBanner) }
